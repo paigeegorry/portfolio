8f509e93b43b40b82b9d978faf7935581fa8521b
Paige Gorry <paigeegorry@gmail.com>
2019-03-17 16:54:08 -0700

    finished redux integration, added marvel project

--- src/services/projects.js
+++ src/services/projects.js
@@ -1,7 +1,7 @@
 export const getProjects = () => {
   return Promise.all([
     {
-      img: 'https://res.cloudinary.com/dkrup6iyl/image/upload/v1552801785/screenshot.png',
+      img: 'https://res.cloudinary.com/dkrup6iyl/image/upload/v1552866457/Screen_Shot_2019-03-17_at_4.46.50_PM.png',
       title: 'SimAlchemy',
       subtitle: 'A choose your own code school adventure for the command line.',
       github: 'https://github.com/team-sailboat/SimAlchemy',
@@ -9,7 +9,15 @@ export const getProjects = () => {
       tech: 'Node.js, MongoDB, Express, Jest'
     },
     {
-      img: 'https://res.cloudinary.com/dkrup6iyl/image/upload/v1552802094/Screen_Shot_2019-03-16_at_10.54.02_PM.png',
+      img: 'https://res.cloudinary.com/dkrup6iyl/image/upload/v1552864945/Screen_Shot_2019-03-17_at_4.21.24_PM.png',
+      title: 'Marvel SuperSearch',
+      subtitle: 'An interactive search for your favorite Marvel Superheroes!',
+      github: 'https://github.com/two-time-fool/urban-waffle',
+      site: 'https://github.com/two-time-fool/urban-waffle',
+      tech: 'React, Redux, Marvel API'
+    },
+    {
+      img: 'src/assets/are-you-a-wizard.png',
       title: 'Are You A Wizard?',
       subtitle: 'An interactive Harry-Potter-themed personality quiz where you can learn your wizarding world fate from start to finish!',
       github: 'https://github.com/Team-8-ball/team-8-ball.github.io',
@@ -17,20 +25,12 @@ export const getProjects = () => {
       tech: 'HTML, CSS, JavaScript'
     },
     {
-      img: 'https://res.cloudinary.com/dkrup6iyl/image/upload/v1552802520/screenshot-1.png',
+      img: 'https://res.cloudinary.com/dkrup6iyl/image/upload/v1552866813/Screen_Shot_2019-03-17_at_4.52.18_PM.png',
       title: 'Planet Photo Album',
       subtitle: 'First static React build showcasing some pretty planets from our lovely galaxy. (Yes, I included Pluto.)',
       github: 'https://github.com/paigeegorry/photo-album',
       site: 'https://vigilant-stonebraker-f8104c.netlify.com/',
       tech: 'React, CSS'
-    },
-    {
-      img: 'https://res.cloudinary.com/dkrup6iyl/image/upload/v1552803871/Screen_Shot_2019-03-16_at_11.05.25_PM.png',
-      title: 'Markdownly',
-      subtitle: 'A markdown editor that allows users to add, edit, and review markdown files.',
-      github: 'https://github.com/team-paige-kristin-dee/markdownly',
-      site: 'https://quirky-agnesi-ac843b.netlify.com/',
-      tech: 'React, Redux, CSS, Jest'
     }
   ]);
 };
