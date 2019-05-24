117d863eebf5addcd30c46420f55acd8416f6a1e
Paige Gorry <paigeegorry@gmail.com>
2019-05-24 14:36:14 -0700

    added favicon!

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import { render } from 'react-dom';
 import App from './components/app/App';
 import store from './store';
 import { Provider } from 'react-redux';
+import Favicon from 'react-favicon';
 
 render(
   <Provider store={store}>
+    <Favicon url="https://res.cloudinary.com/dkrup6iyl/image/upload/v1558733722/dev-icon.png" />,
     <App />
   </Provider>,
   document.getElementById('root')
