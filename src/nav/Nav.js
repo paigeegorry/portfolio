8d8fd6bea873c367584ffcf64c84c34acccb9d98
Paige Gorry <paigeegorry@gmail.com>
2020-02-28 12:00:35 -0800

    brought in hamnburger menu

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import { styles } from './Nav.styles';
+import { SocialIcon } from 'react-social-icons';
 
 export default function Nav() {
   return (
-    <nav>
-    <div class="nav-wrapper">
-      <a href="/" class="brand-logo">Logo</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
-    </div>
-  </nav>
-  )
+    <Menu styles={styles}>
+      <a id="home" className="menu-item" href="/">Home</a>
+      <a id="projects" className="menu-item" href="/projects">Projects</a>
+      <a id="about" className="menu-item" href="/about">About</a>
+      <span className="social" style={{marginTop: '18px'}}>
+        <SocialIcon url="https://github.com/paigeegorry" network="github" bgColor="#ffff" style={{ height: 25, width: 25 }}  />
+        <SocialIcon url="mailto: paigeegorry@gmail.com" network="email" bgColor="#ffff" style={{ height: 25, width: 25 }}  />
+        <SocialIcon url="https://twitter.com/paigeegorry" network="twitter" bgColor="#ffff" style={{ height: 25, width: 25 }}  />
+        <SocialIcon url="https://linkedin.com/in/paigeegorry" network="linkedin" bgColor="#ffff" style={{ height: 25, width: 25 }}  />
+      </span>
+    </Menu>
+  );
 }
---
+++ src/nav/Nav.styles.js
@@ -0,0 +1,51 @@
+export const styles = {
+  bmBurgerButton: {
+    position: 'absolute',
+    width: '36px',
+    height: '30px',
+    left: '36px',
+    top: '36px'
+  },
+  bmBurgerBars: {
+    background: '#373a47'
+  },
+  bmBurgerBarsHover: {
+    background: '#a90000'
+  },
+  bmCrossButton: {
+    height: '24px',
+    width: '24px'
+  },
+  bmCross: {
+    background: '#bdc3c7'
+  },
+  bmMenuWrap: {
+    position: 'fixed',
+    height: '100%',
+    top: 0,
+  },
+  bmMenu: {
+    background: '#2B2D2F',
+    padding: '2.5em 1.5em 0',
+    fontSize: '1.15em'
+  },
+  bmMorphShape: {
+    fill: '#2B2D2F',
+  },
+  bmItemList: {
+    color: '#ffff',
+    padding: '0.8em',
+    display: 'flex',
+    flexDirection: 'column',
+    background: 'none',
+  },
+  bmItem: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  bmOverlay: {
+    top: 0,
+    background: 'rgba(0, 0, 0, 0.3)'
+  },
+}
